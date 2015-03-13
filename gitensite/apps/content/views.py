ee5ddf949e08f40a417b742cb93a4fa6c582c28c
Seth Woodworth <seth@sethish.com>
2015-03-13 17:20:30 -0400

    adding basic content app for homepage

---
+++ gitensite/apps/content/views.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        return context
--- gitensite/urls.py
+++ gitensite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from gitensite.apps.content.views import HomePageView
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
 ]
