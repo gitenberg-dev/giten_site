8b57e36209b383d03f0fc256cf200340fdb0bf54
Seth Woodworth <seth@sethish.com>
2015-03-14 01:51:13 -0400

    adding staticfile serving to heroku

--- gitensite/urls.py
+++ gitensite/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
 
 from gitensite.apps.content.views import HomePageView
 from gitensite.apps.content.views import NewsletterView
@@ -10,3 +11,8 @@ urlpatterns = [
     url(r'^newsletter/(?P<issue>\d)$', NewsletterView.as_view(), name='newsletter'),
     url(r'^$', HomePageView.as_view(), name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
