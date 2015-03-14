125e39510ee5294115f2efbbd066459d2beca8ea
Seth Woodworth <seth@sethish.com>
2015-03-14 01:17:49 -0400

    update content app for frontpage and newsletter via sciidoc

--- gitensite/apps/content/views.py
+++ gitensite/apps/content/views.py
@@ -10,3 +10,11 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         return context
+
+class NewsletterView(TemplateView):
+
+    def get_template_names(self, **kwargs):
+        return ['newsletters/{issue}.html'.format(
+                issue=str(self.kwargs['issue'])
+                )]
+
