67ff904138cb054dd22cf9403b016ee1ed8035b3
Seth Woodworth <seth@sethish.com>
2015-03-14 01:18:21 -0400

    adding newsletters url to router

--- gitensite/urls.py
+++ gitensite/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from gitensite.apps.content.views import HomePageView
+from gitensite.apps.content.views import NewsletterView
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^newsletter/(?P<issue>\d)$', NewsletterView.as_view(), name='newsletter'),
     url(r'^$', HomePageView.as_view(), name='home'),
 ]
