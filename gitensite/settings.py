15e22b8c103eb60b96a43ffcb9a23e98db7316fe
Seth Woodworth <seth@sethish.com>
2015-05-08 12:04:36 -0400

    adds static_root

--- gitensite/settings.py
+++ gitensite/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_DIR = '/static'
+STATIC_ROOT = '/static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
