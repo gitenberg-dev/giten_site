44d5e518b5c2c35ec06afc0e7e4f60caa4711ac9
Seth Woodworth <seth@sethish.com>
2015-05-08 12:11:05 -0400

    moves static files to assets dir, static as collect target

--- gitensite/settings.py
+++ gitensite/settings.py
@@ -98,9 +98,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = './static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "assets"),
 )
 
 
