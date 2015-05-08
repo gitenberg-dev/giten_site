661eaa6cb80e9370f0e261ae80b19279c9ca9490
Seth Woodworth <seth@sethish.com>
2015-05-08 15:49:49 -0400

    updates content, fixes #10

--- gitensite/urls.py
+++ gitensite/urls.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 from gitensite.apps.content.views import HomePageView
 from gitensite.apps.content.views import NewsletterView
@@ -15,6 +19,7 @@ urlpatterns = [
     url(r'^books/?$', BookRepoListView.as_view(), name='books'),
     url(r'^get-involved/?$', GetInvolvedView.as_view(), name='get-involved'),
     url(r'^faq/?$', FAQView.as_view(), name='faq'),
+    url(r'^license/?$', TemplateView.as_view(template_name="license.html"), name='license'),
     url(r'^.*$', HomePageView.as_view(), name='home'),
 ]
 
