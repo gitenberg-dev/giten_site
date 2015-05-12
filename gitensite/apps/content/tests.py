b1472b167da605658f1826b3b775bc3de414b5e8
eric <eric@hellman.net>
2015-05-11 22:53:05 -0400

    page tests

--- gitensite/apps/content/tests.py
+++ gitensite/apps/content/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+# invoke with ./manage.py test gitensite.apps.content or ./manage.py test
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+
+class PageTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        
+    def test_view_by_anonymous(self):
+        anon_client = Client()
+        r = anon_client.get("/")
+        self.assertEqual(r.status_code, 200)
+        r = anon_client.get("/newsletter/")
+        self.assertEqual(r.status_code, 200)
+        r = anon_client.get("/books/")
+        self.assertEqual(r.status_code, 200)
+        r = anon_client.get("/get-involved/")
+        self.assertEqual(r.status_code, 200)
+        r = anon_client.get("/faq/")
+        self.assertEqual(r.status_code, 200)
+        r = anon_client.get("/license/")
+        self.assertEqual(r.status_code, 200)
