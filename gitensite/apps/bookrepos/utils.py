a33b50f0f634c71392c7037d5e2c0cc6b1dc2214
Seth Woodworth <seth@sethish.com>
2015-06-15 18:27:04 -0400

    deactivates BookRepoForm which django18 complains about

--- gitensite/apps/bookrepos/utils.py
+++ gitensite/apps/bookrepos/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .forms import BookRepoForm
+# from .forms import BookRepoForm
 from .models import BookRepo
 from .interfaces import GithubToBookRepoInterface
 
