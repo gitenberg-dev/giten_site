b01ee5332cf3234b1d4e05fd67cc28f2ab8db6e1
Seth Woodworth <seth@sethish.com>
2015-06-15 18:27:35 -0400

    removing django-dotenv, we don't use it

--- gitensite/wsgi.py
+++ gitensite/wsgi.py
@@ -5,14 +5,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 import os
-import dotenv
 
-
-try:
-    dotenv.read_dotenv(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-except Exception as e:
-    print(e)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitensite.settings')
 
