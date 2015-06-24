7e32b13b35288cf664371f32d9b9b28eb508e16c
Seth Woodworth <seth@sethish.com>
2015-06-23 21:07:11 -0400

    refactor book fetching generator objects

--- gitensite/apps/bookrepos/utils.py
+++ gitensite/apps/bookrepos/utils.py
@@ -4,21 +4,15 @@ import getpass
 import logging
 import time
 
-from github3 import login
 
 from gitensite.apps.bookrepos.models import GitHubAuthToken
 # from .forms import BookRepoForm
 # from .models import BookRepo
 from .interfaces import GithubToBookRepoInterface
 from .models import BookRepo
+from .models import GitHubAuthToken
 
 logger = logging.getLogger(__name__)
-
-class GitHubOrg(object):
-    """docstring for GitHubOrg"""
-    def __init__(self, arg):
-        super(GitHubOrg, self).__init__()
-        self.arg = arg
 
 class CommitTree():
     """
@@ -29,74 +23,87 @@ class CommitTree():
         self.tree = tree
         self.dict = self.tree.as_dict()
 
-class BookRepoGenerator():
+
+class SearchAllRepos():
     def __init__(self):
-        self.gh = self.get_or_create_auth()
+        self.gh = GitHubAuthToken.get_token_or_login()
+
+    def run(self):
+        for repo in self.gh.repositories_by('gitenberg'):
+            interface = GithubToBookRepoInterface(repo)
+            book_repo = interface.fulfill()
+            logger.debug(interface)
+            book_repo.save()
+
+
+
+
+class BookRepoGenerator():
+    def __init__(self, start=1, count=10):
+        self.gh = GitHubAuthToken.get_token_or_login()
         self.check_ratelimit()
-        self.org = self.gh.organization('gitenberg')
+        # range(n..max(PG id)), search gh with user:GITenberg
+
+    def search_repos(self):
+        """ Search the list of GITenberg repos
+        sort by most recent"""
+        pass
+
+    def get_bookrepo(self, pg_id):
+        """ takes `pg_id` a PG book id
+        searches github for that id
+        returns github repository object """
+        search_prefix = "user:GITenberg"
+        search_id = "_{}".format(pg_id)
+        search_str = " ".join([search_prefix, search_id])
+
+        result_iterator = self.gh.search_repositories(search_str)
+        result = result_iterator.next()  # fetching the first result from search
+        logger.debug(result)
+        return result.repository
 
     def check_ratelimit(self):
-        # Check to see if we have enough ratelimits to continue, or if we
-        # should wait until timestamp
+        """ Check to see if we have enough ratelimits to continue, or if we
+        should wait until timestamp
+        Check on each run of run to see if we have >100 rate limits remaining
+        if we do not, fetch the timestamp for when we can begin again """
+        # TODO: break if not enough ratelimits remaining
         self.rates = self.gh.rate_limit()
         logger.debug("\tGitHub api allocation {0}".format(self.rates['resources']['core']['remaining']))
         logger.debug("\t{0} seconds until refresh".format(
             self.rates['resources']['core']['reset'] -
             int(time.time())
         ))
-        # if rates['resources']['core']['remaining'] < 10000:
-        #     # TODO: wait until
-        #     reset = rates['resources']['core']['reset']
-        #     now = calendar.timegm(datetime.datetime.utcnow())
-        #     import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
 
-    def get_or_create_auth(self):
-        try:
-            gh_token = GitHubAuthToken.objects.get(id=1)
-            gh = login(token=gh_token.token)
-            logger.debug("Recovered gh token from db, used that to log in.")
-        except GitHubAuthToken.DoesNotExist:
-            self.stdout.write('GitHub Token not found, try running make_github_token')
-            self.stdout.write('I need to log into your account:')
-
-            gh = login(raw_input("username:"), password=getpass.getpass())
-
-        return gh
-
-    def run(self, save=True, max_count=100):
-
-        # Find the latest BookRepo we updated in the db, and get its etag
-        latest_updated_book_repo = BookRepo.objects.order_by('-updated_at')[0]
-        logger.debug("Latest updated book: {0}".format(latest_updated_book_repo))
-        latest_etag = latest_updated_book_repo.etag
-
-        """ Bad news,
-        etag doesn't paginate each `number` bounded request to org.repositories()
-        so we don't need to check etag in the db.
-        """
-        # https://developer.github.com/v3/#rate-limiting
-        # Check on each run of run to see if we have >100 rate limits remaining
-        # if we do not, fetch the timestamp for when we can begin again
-
-        # if not latest_etag:
-        #     logger.debug("Latest etag: {0}. providing that to org.repositories()".format(latest_etag))
-        #     repo_iterator = self.org.repositories(etag=latest_etag)
-        # else:
-            # iterate over
-            # logger.debug("we have an etag to iterate from with {0}".format(repo_iterator))
-
-        repo_iterator = self.org.repositories()
-        for repo in repo_iterator:
+    def run(self):
+        for pg_id in self.todo:
+            repo = self.get_bookrepo(pg_id)
 
             logger.debug("We have a repo from our iterator to digest")
-            self.etag = repo_iterator.etag
-            # TODO: fetch existing book as update form
-
             # Create an object that manages transfering data from a
             # Github3.Repo to a models.BookRepo
-            interface = GithubToBookRepoInterface(repo, self.etag)
+            interface = GithubToBookRepoInterface(repo)
             book_repo = interface.fulfill()
-            print interface
+            logger.debug(interface)
             book_repo.save()
 
         self.run()
+
+class BookRepoSearchAllGenerator(BookRepoGenerator):
+    def __init__(self, page=None):
+        """ Search for all user:GITenberg and paginate/save
+        as bookrepos
+        """
+
+class SearchOneGenerator(BookRepoGenerator):
+    def __init__(self, page=None):
+        """ Search for all user:GITenberg and paginate/save
+        as bookrepos
+        """
+        self.todo = range(start, count)
+
+def get_latest_etag():
+    # Not using this at the moment.
+    latest_updated_book_repo = BookRepo.objects.order_by('-updated_at')[0]
+    logger.debug("Latest updated book: {0}".format(latest_updated_book_repo))
+    latest_etag = latest_updated_book_repo.etag
