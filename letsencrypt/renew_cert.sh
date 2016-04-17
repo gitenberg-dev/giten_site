3efe414d9ea72ed6eef59df002dc7d1de417bdcd
Paul Moss <moss.paul@gmail.com>
2016-04-17 14:40:10 +0000

    Make sure we handle the renewal steps properly

--- letsencrypt/renew_cert.sh
+++ letsencrypt/renew_cert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curdate=$(date +"%Y-%m-%d")
+
 # Make sure we've got the previous certificate
 if [ ! -d /etc/letsencrypt ]; then
     echo "Existing certificate not found on server.  Pulling a backup copy from S3"
@@ -26,11 +28,20 @@ PROJECT_DIR="/opt/python/current/app"
 if [ $? -ne 0 ]; then
     echo "An error occurred with the letsencrypt cert process."
 else
-    echo "Successfully renewed the certificate.  Upload it to IAM."
-    # I think this command will have to be different the second time this is run
-    aws iam upload-server-certificate --server-certificate-name gitenberg-lencrypt --certificate-body file:///etc/letsencrypt/live/www.gitenberg.org/cert.pem --private-key file:///etc/letsencrypt/live/www.gitenberg.org/privkey.pem --certificate-chain file:///etc/letsencrypt/live/www.gitenberg.org/chain.pem
-    echo "Upload Certs to S3."
-    aws s3 cp --recursive /etc/letsencrypt s3://lencrypt/
+    echo "Successfully renewed the certificate.  Upload to AWS IAM."
+    aws iam upload-server-certificate --server-certificate-name gitenberg-lencrypt-${curdate} --certificate-body file:///etc/letsencrypt/live/www.gitenberg.org/cert.pem --private-key file:///etc/letsencrypt/live/www.gitenberg.org/privkey.pem --certificate-chain file:///etc/letsencrypt/live/www.gitenberg.org/chain.pem | tee /tmp/aws_upload.response
+    if [ $? -ne 0 ]; then
+        echo "An error occurred uploading the certificate to AWS IAM"
+    else
+        cert_arn=$(grep 'Arn' /tmp/aws_upload.response | sed -e "s/^.*\"arn:/arn:/" -e "s/\",\s*$//")
+        echo "Found ARN ${cert_arn} for uploaded certificate"
+        # ARN contains a / character, so use alternate separators for sed command
+        sed -e "s~REPLACEME~${cert_arn}~" /tmp/arn_options.json > /tmp/arn_options_${curdate}.json
+        echo "Update environment configuration with the new certificate"
+        aws elasticbeanstalk update-environment --environment-name giten-site-dev --option-settings file:///tmp/arn_options_${curdate}.json
+        echo "Upload Certs to S3 for future reference"
+        aws s3 cp --recursive /etc/letsencrypt s3://lencrypt/
+    fi
 fi
 
 exit 0
