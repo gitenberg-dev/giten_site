3a512d325264132a5ffe61dd1808e77f52103f90
Paul Moss <moss.paul@gmail.com>
2016-04-20 10:47:25 -0400

    Don't enrich in debug mode

--- gitensite/apps/bookinfo/management/commands/load_repos.py
+++ gitensite/apps/bookinfo/management/commands/load_repos.py
@@ -3,6 +3,7 @@ import csv
 import requests
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from gitensite.apps.bookinfo.models import Book
 from gitenberg.util.catalog import BookMetadata, repo_list
@@ -11,6 +12,10 @@ class Command(BaseCommand):
     help = "load ids and repo names from Raymond's text TSV file"
     args = "rdf_library", "start"
     def handle(self, rdf_library, start=0, *args, **kwargs):
+        should_enrich = True
+        if settings.DEBUG:
+            should_enrich = False
+
         start = int(start)
         if start==0:
             Book.objects.all().delete()
@@ -19,7 +24,7 @@ class Command(BaseCommand):
                 continue
             try:
                 (book,created) = Book.objects.get_or_create(book_id=int(pg_id), repo_name=repo_name)
-                metadata=BookMetadata(book,rdf_library=rdf_library, enrich=True)
+                metadata=BookMetadata(book,rdf_library=rdf_library, enrich=should_enrich)
                 book.language = metadata.language if isinstance(metadata.language,str) else 'mul'
                 book.description = metadata.description
                 book.title = metadata.title
