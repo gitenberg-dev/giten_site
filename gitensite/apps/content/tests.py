1167df332d130c3f2ff4e95faeb94288e63d95b8
Brandon Rothweiler <brandonrothweiler@gmail.com>
2018-04-04 15:07:36 -0400

    Add test for external links

--- gitensite/apps/content/tests.py
+++ gitensite/apps/content/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.test.client import Client
 
 from gitensite.apps.bookinfo.models import Author, Book, Cover
+from gitensite.apps.bookinfo.external import getExternalLinks
 
 import os
 
@@ -29,44 +30,48 @@ class PageTests(TestCase):
         r = anon_client.get("/license/", follow=True)
         self.assertEqual(r.status_code, 200)
 
-        yaml = """_repo: testbook
+        yaml = """_repo: Alice-s-Adventures-in-Wonderland_11
+alternative_title: Alice in Wonderland
 creator:
   author:
-    agent_name: Austen, Jane
-    birthdate: 1775
-    deathdate: 1817
-    gutenberg_agent_id: '68'
-    url: http://www.gutenberg.org/2009/agents/68
-    wikipedia: http://en.wikipedia.org/wiki/Jane_Austen
-gutenberg_bookshelf:
-  - Best Books Ever Listings
-  - Harvard Classics
-gutenberg_issued: '1998-06-01'
+    agent_name: Carroll, Lewis
+    alias: Dodgson, Charles Lutwidge
+    birthdate: 1832
+    deathdate: 1898
+    gutenberg_agent_id: 7
+    url: http://www.gutenberg.org/2009/agents/7
+    wikipedia: http://en.wikipedia.org/wiki/Lewis_Carroll
+description: "See also our HTML edition: #928"
+gutenberg_bookshelf: Children's Literature
+gutenberg_issued: 2008-06-27
 gutenberg_type: Text
 identifiers:
-  gutenberg: '1234223423'
+  gutenberg: 11
 language: en
 publisher: Project Gutenberg
 rights: Public domain in the USA.
 rights_url: http://creativecommons.org/about/pdm
 subjects:
-  - !lcsh Love stories
-  - !lcc PR
-  - !lcsh Domestic fiction
-  - !lcsh Young women -- Fiction
-  - !lcsh England -- Fiction
-  - !lcsh Social classes -- Fiction
-  - !lcsh Courtship -- Fiction
-  - !lcsh Sisters -- Fiction
-title: Pride and Prejudice
-url: http://www.gutenberg.org/ebooks/1342"""
+- !lcc PR
+- !lcsh Fantasy
+- !lcc PZ
+title: Alice's Adventures in Wonderland
+url: http://www.gutenberg.org/ebooks/11"""
 
+        #Test submitting a book to the database via the POST route
         r = anon_client.post("/books/post/", data=yaml, content_type='application/octet-stream', secure=True)
         self.assertEqual(r.status_code, 200)  
+        
+        #Test retrieving a book from the database by title
+        book = Book.objects.get(title="Alice's Adventures in Wonderland")
+        self.assertEqual(book.title, "Alice's Adventures in Wonderland")
 
-        book = Book.objects.get(title="Pride and Prejudice")
-        self.assertEqual(book.title, "Pride and Prejudice")
+        #Test retrieving an author from the database by name
+        author = Author.objects.get(name="Carroll, Lewis")
+        self.assertEqual(author.name, "Carroll, Lewis")
 
-        author = Author.objects.get(name="Austen, Jane")
-        self.assertEqual(author.name, "Austen, Jane")
+        #Test external links feature
+        externalLinks = getExternalLinks(book)
+        self.assertTrue("Librivox" in externalLinks)
+        self.assertTrue("Standard Ebooks" in externalLinks)
         
