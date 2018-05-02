64b4d5dfc8f15606355f104d3292804d14dbd65f
Brandon Rothweiler <brandonrothweiler@gmail.com>
2018-05-02 18:24:24 -0400

    check status code when downloading covers

--- gitensite/apps/bookinfo/db.py
+++ gitensite/apps/bookinfo/db.py
@@ -26,16 +26,17 @@ def addBookFromYaml(yaml):
                 #Upload cover to S3
                 url = urlparse.urljoin("https://raw.githubusercontent.com/GITenberg/" + obj["_repo"] + "/master/", cover["image_path"])
                 r = requests.get(url)
-                contentfile = ContentFile(r.content)
-                uploadpath = obj["_repo"] + ".png"
+                if r.status_code == 200:
+                    contentfile = ContentFile(r.content)
+                    uploadpath = obj["_repo"] + ".png"
 
-                #Add cover to database
-                coverobject = Cover.objects.create(
-                    book=book,
-                    default_cover=(num_existing_covers == 0)
-                )
-                coverobject.file.save(uploadpath, contentfile)
-                coverobject.file.close()
+                    #Add cover to database
+                    coverobject = Cover.objects.create(
+                        book=book,
+                        default_cover=(num_existing_covers == 0)
+                    )
+                    coverobject.file.save(uploadpath, contentfile)
+                    coverobject.file.close()
 
     creator = None
     if "creator" in obj:
