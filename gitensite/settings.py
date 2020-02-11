87a576157c9d2ad8118c494443a1e70b9a156091
eric <eric@hellman.net>
2020-02-11 09:35:32 -0500

    add allowed hosts

--- gitensite/settings.py
+++ gitensite/settings.py
@@ -20,7 +20,11 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'gitensite.elasticbeanstalk.com',
+    'giten-site.us-east-1.elasticbeanstalk.com',
+]
 
 # Application definition
 COMMON_APPS = [
