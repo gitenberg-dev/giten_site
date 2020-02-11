eaf418b33a6b486a064472259d9720a1051b3d7a
eric <eric@hellman.net>
2020-02-11 12:42:56 -0500

    Update settings.py

--- gitensite/settings.py
+++ gitensite/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = [
     'localhost',
     'gitensite.elasticbeanstalk.com',
     'giten-site.us-east-1.elasticbeanstalk.com',
+    'gitenberg.us-east-1.elasticbeanstalk.com',
     '172.31.49.28',
 ]
 
