de998578702d0a5606e6148fde02a1e7acdd5cc0
eric <eric@hellman.net>
2020-02-11 13:17:12 -0500

    allowed =*

--- gitensite/settings.py
+++ gitensite/settings.py
@@ -20,15 +20,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',False))
 
-ALLOWED_HOSTS = [
-    'gitenberg.org',
-    'www.gitenberg.org',
-    'localhost',
-    '127.0.0.1',
-    'gitensite.elasticbeanstalk.com',
-    'giten-site.us-east-1.elasticbeanstalk.com',
-    'gitenberg.us-east-1.elasticbeanstalk.com',
-]
+ALLOWED_HOSTS = ['*']
 USE_X_FORWARDED_HOST = True
 
 # Application definition
